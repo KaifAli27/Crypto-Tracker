1e31ca6dd0415d62d0466cc4172be567086ff348
Kaif Ali <kaifali@cmail.carleton.ca>
2024-07-27 23:32:10 +0400

    crypto table implementation

--- src/context/CoinContext.jsx
+++ src/context/CoinContext.jsx
@@ -1,8 +1,36 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 export const CoinContext = createContext();
 
 const CoinContextProvider = (props)=>{
-    const contextValue = {}
+
+    const [allCoin, setAllCoin] = useState([]);
+    const [currency, setCurrency] = useState({
+        name: "cad",
+        symbol: "$"
+    })
+
+    const fetchAllCoin = async ()=>{
+        const options = {
+            method: 'GET',
+            headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-89jmyfuovdNAyyNWwtmuaMzd'}
+          };
+          
+          fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`, options)
+            .then(response => response.json())
+            .then(response => setAllCoin(response))
+            .catch(err => console.error(err));
+
+    }
+
+useEffect(()=>{
+    fetchAllCoin();
+},[currency])
+
+    const contextValue = {
+        allCoin, currency, setCurrency
+
+    }
+    
     return(
         <CoinContext.Provider value={contextValue}>
             {props.children}
