be84e0565bddbcc81c5b2a906dc5468910af8c5e
Kaif Ali <kaifali@cmail.carleton.ca>
2024-07-28 23:50:17 +0400

    what if 2.0

---
+++ src/pages/What-If/Whatif.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Whatif.css'
+import { CoinContext } from '../../context/CoinContext'
+
+const Whatif = () => {
+    
+        return (
+          <div className='spinner2'>
+            <div className="what">
+                <h1>Page Under Construction</h1>
+                <p>This is a fun calculator to see how much you could have made by investing into Cryptcurrencies in the early days.</p>
+                <p>Select the Crypto and Year from the below menu and choose the investment amount, ranging from just the price of a cup of coffee to the average car.</p>
+            </div>
+            
+            <div className="spin2">
+                
+            </div>
+          </div>
+        )
+      }
+
+
+export default Whatif
